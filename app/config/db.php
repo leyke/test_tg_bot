d97eaead13011a1171303cabfd208478e7abad93
Artem Makurkov <oven1995@gmail.com>
2025-04-02 22:44:06 +0300

    Поднял yii до конца в докере

--- app/config/db.php
+++ app/config/db.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'class'    => 'yii\db\Connection',
+    'dsn'      => env('DB_DSN'),
+    'username' => env('DB_USERNAME'),
+    'password' => env('DB_PASSWORD'),
+    'charset'  => env('DB_CHARSET', 'utf8mb4'),
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
